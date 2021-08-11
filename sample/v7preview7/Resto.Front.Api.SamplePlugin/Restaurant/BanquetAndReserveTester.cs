4f117a24f39c09012979be681dcd4e4afa48832f
Airat Salikhov <asalikhov@iiko.ru>
2021-08-11 14:03:53 +0300

    RMS-52779 Publish V7Preview7 samples

---
+++ sample/v7preview7/Resto.Front.Api.SamplePlugin/Restaurant/BanquetAndReserveTester.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Threading;
+using System.Windows;
+
+namespace Resto.Front.Api.SamplePlugin.Restaurant
+{
+    internal sealed class BanquetAndReserveTester : IDisposable
+    {
+        private readonly object syncObject = new object();
+        private readonly CompositeDisposable resources = new CompositeDisposable();
+        private bool disposed;
+
+        public BanquetAndReserveTester()
+        {
+            var windowThread = new Thread(EntryPoint);
+            windowThread.SetApartmentState(ApartmentState.STA);
+            windowThread.Start();
+            PluginContext.Log.Info("BanquetAndReserveTester started");
+        }
+
+        private void EntryPoint()
+        {
+            Window window;
+            lock (syncObject)
+            {
+                if (disposed)
+                    return;
+
+                window = new Window
+                {
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                    Content = new BanquetAndReserveView(),
+                    Title = GetType().Name,
+                    Topmost = true
+                };
+
+                resources.Add(Disposable.Create(() =>
+                {
+                    window.Dispatcher.InvokeShutdown();
+                    window.Dispatcher.Thread.Join();
+                }));
+            }
+            PluginContext.Log.Info("Show BanquetAndReserveView dialog...");
+            window.ShowDialog();
+            PluginContext.Log.Info("Closed BanquetAndReserveView dialog...");
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+            lock (syncObject)
+            {
+                resources.Dispose();
+                PluginContext.Log.Info("BanquetAndReserveTester stopped");
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ sample/v7preview7/Resto.Front.Api.SampleScalePlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Resto.Front.Api.SampleScalePlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Resto.Front.Api.SampleScalePlugin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyCompany("iiko")]
+[assembly: AssemblyCopyright("Copyright © iiko")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
